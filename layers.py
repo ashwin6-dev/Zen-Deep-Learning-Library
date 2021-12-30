fixed softmax derivative

layers.py
@@ -38,8 +38,8 @@ class Linear(Layer):
     def __call__(self, x):
         self.input = x
         if not self.initialized:
-            self.w = np.random.randn(self.input.shape[-1], self.units)
-            self.b = np.random.randn(self.units)
+            self.w = np.random.rand(self.input.shape[-1], self.units)
+            self.b = np.random.rand(self.units)
             self.initialized = True
 
         return self.input @ self.w + self.b
@@ -68,13 +68,19 @@ class Relu(Activation):
 
 class Softmax(Activation):
     def __call__(self, x):
-        self.output = np.exp(x) / np.sum(np.exp(x))
+        self.input = x
+        exps = np.exp(x - np.max(x))
+        self.output = exps / np.sum(exps, axis=1, keepdims=True)
         return self.output
     
     def backward(self, grad):
-        reshape_output = self.output.reshape(-1, 1)
-
-        return grad * (np.diagflat(reshape_output) - np.dot(reshape_output, reshape_output.T))
+        m, n = self.output.shape
+        p = self.output
+        tensor1 = np.einsum('ij,ik->ijk', p, p)  # (m, n, n)
+        tensor2 = np.einsum('ij,jk->ijk', p, np.eye(n, n))  # (m, n, n)
+        dSoftmax = tensor2 - tensor1
+        dz = np.einsum('ijk,ik->ij', dSoftmax, grad)
+        return dz
 
 class Tanh(Activation):
     def __call__(self, x):

main.py
@@ -5,16 +5,16 @@ import numpy as np
 
 
 x = np.array([[0, 1], [0, 0], [1, 1], [0, 1]])
-y = np.array([[1], [0], [0], [1]]) 
+y = np.array([[0, 1], [1, 0], [1, 0], [0, 1]]) 
 
 net = layers.Model([
     layers.Linear(8),
     layers.Linear(4),
     layers.Sigmoid(),
-    layers.Linear(1),
-    layers.Sigmoid()
+    layers.Linear(2),
+    layers.Softmax()
 ])
 
-net.train(x, y, optim=optim.SGD(lr=0.01), loss=loss.MSE(), epochs=100)
+net.train(x, y, optim=optim.SGD(lr=0.9), loss=loss.MSE(), epochs=100)
 
 print (net(x))